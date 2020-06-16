7470f28121ec259a8de6e0ccbfb539c40ae0038d
Vycktor Hugo Stark <huho387@gmail.com>
2020-06-16 16:42:05 -0400

    Create lang.py
    Excuse me, I forgot to commit this

---
+++ lang.py
@@ -0,0 +1,66 @@
+languages = {
+	"en": [{ #ingles
+		'about': '''SimSimi (chim chim, Korean for "심심", meaning bored) is a talk set created in 2002 by ISMaker
+On the telegram, simsimi is being maintained by @VycktorStark through the <a href='http://developer.simsimi.com/api'> api-simsimi </a> which it charges based on usage.
+© <a href='http://developer.simsimi.com'> SimSimi Inc. 2013 </a>''',
+		'start':"Hello, My name is Simsimi! I'm here to talk!\nBut take it easy I'm in the development phase.",
+		"notcmdia":"This command is no longer available, you can now recognize your language automatically - send /about know more about me!"
+		}],
+	"ar": [{ #árabe 
+		'about': '''SimSimi (chim chim ، كوري لـ "심심" ، يعني بالملل) هي مجموعة نقاش تم إنشاؤها في عام 2002 بواسطة ISMaker
+في البرقية ، تتم صيانة simsimi بواسطةVycktorStark من خلال <a href='http://developer.simsimi.com/api'> api-simsimi </a> الذي يتم تحصيله بناءً على الاستخدام.
+© <a href='http://developer.simsimi.com'> SimSimi Inc. 2013 </a>''',
+		'start':"Oمرحبا اسمي سيمسيمي! أنا هنا لأتحدث!\n ولكن خذ الأمور بسهولة فأنا في مرحلة التطوير",
+		"notcmdia":"لم يعد هذا الأمر متاحًا ، يمكنك الآن التعرف على لغتك تلقائيًا - إرسال / حول معرفة المزيد عني!"
+		}],
+	"es": [{ #espanhol
+		'about': '''SimSimi (chim chim, coreano para "심심", que significa aburrido) es un set de conversación creado en 2002 por ISMaker.
+En el telegrama, @VycktorStark mantiene simsimi a través de <a href='http://developer.simsimi.com/api'> api-simsimi </a> que cobra en función del uso.
+© <a href='http://developer.simsimi.com'> SimSimi Inc. 2013 </a>''',
+		'start':"Hola, mi nombre es Simsimi! Estoy aqui para hablar!\nPero tómalo con calma, estoy en la fase de desarrollo.",
+		"notcmdia":"Este comando ya no está disponible, ahora puede reconocer su idioma automáticamente: ¡envíe /about más sobre mí!"
+		}],
+	"fr": [{ #frances
+		'about': '''SimSimi (chim chim, coréen pour "심심", signifiant s'ennuyer) est un talk-show créé en 2002 par ISMaker
+Sur le télégramme, simsimi est maintenu par @VycktorStark via le <a href='http://developer.simsimi.com/api'> api-simsimi </a> qu'il facture en fonction de l'utilisation.
+© <a href='http://developer.simsimi.com'> SimSimi Inc. 2013 </a>''',
+		'start':"Bonjour, je m'appelle Simsimi! Je suis ici pour parler!\nMais tranquille, je suis en phase de développement.",
+		"notcmdia":"Cette commande n'est plus disponible, vous pouvez maintenant reconnaître automatiquement votre langue - envoyez /about en savoir plus sur moi!"
+		}],
+	"it": [{ #italiano
+		'about': '''SimSimi (chim chim, coreano per "심심", che significa annoiato) è un talk set creato nel 2002 da ISMaker
+Nel telegramma, simsimi è gestito da @VycktorStark attraverso <a href='http://developer.simsimi.com/api'> api-simsimi </a> che carica in base all'utilizzo.
+© <a href='http://developer.simsimi.com'> SimSimi Inc. 2013 </a>''',
+		'start':"Ciao, mi chiamo Simsimi! Sono qui per parlare!\nMa prenditi la calma, sono in fase di sviluppo.",
+		"notcmdia":"Questo comando non è più disponibile, ora puoi riconoscere automaticamente la tua lingua - invia /about per saperne di più su di me!"
+		}],
+	"ja": [{ #japones
+		'about': '''SimSimi（チムチム、韓国語で「심심」、退屈を意味）は、ISMakerによって2002年に作成されたトークセットです。
+電報では、simsimiは@VycktorStarkによって<a href='http://developer.simsimi.com/api'> api-simsimi </a>を通じて維持され、使用量に基づいて課金されます。
+©<a href='http://developer.simsimi.com'>SimSimi Inc. 2013</a>''',
+		'start':"こんにちは、私の名前はシムシミです！ 私はここに話しに来ました！\nしかし、私は開発段階にいるので安心してください.",
+		"notcmdia":"このコマンドは使用できなくなり、言語を自動的に認識できるようになりました。「/について」を送信して、私のことをもっと知ることができます！"
+		}],
+	"ko": [{ #Coreano
+		'about': '''S심심이 (심심을 의미하는 침팬지, 심심)는 2002 년에 ISMaker가 만든 대화입니다.
+텔레 그램에서 @VycktorStark는 simsimi를 <a href='http://developer.simsimi.com/api'> api-simsimi </a>를 통해 유지 관리하며 사용량에 따라 요금을 청구합니다.
+© <a href='http://developer.simsimi.com'> SimSimi Inc. 2013 </a>''',
+		'start':"안녕하세요, Simsimi입니다! 얘기하러 왔습니다!\n하지만 개발 단계에있는 편이 낫다.",
+		"notcmdia":"이 명령은 더 이상 사용할 수 없습니다. 이제 언어를 자동으로 인식 할 수 있습니다. /about 을 보내서 자세한 정보를 얻으십시오!"
+		}],
+	"pt": [{ #portugues
+		'about': """SimSimi (chim-chími, coreano de "심심", que significa entediado) é um chatterbot criado em 2002 pela ISMaker
+No telegram, o simsimi está sendo mantido pelo @VycktorStark atraves da <a href='http://developer.simsimi.com/api'>api-simsimi</a> que cobra com base no uso.
+© <a href='http://developer.simsimi.com'>SimSimi Inc. 2013</a>""",
+		'start':"Olá, Me chamo Simsimi! estou aqui para conversar!\nMas, vai com calmar estou em fase de desenvolvimento.",
+		'notcmdia': "Este comando não está mais disponível, agora eu posso reconhecer seu idioma automaticamente - envie /about saber mais sobre mim!"
+		}],
+	"de": [{ #dinamarquês
+		'about': '''SimSimi (chim-chími, koreansk for "심심", hvilket betyder keder sig) er en snakkesæt oprettet i 2002 af ISMaker
+I telegrammet vedligeholdes simsimi af @VycktorStark gennem <a href='http://developer.simsimi.com/api'> api-simsimi </a>, som den opkræver baseret på brug.
+© <a href='http://developer.simsimi.com'>SimSimi Inc. 2013</a>''',
+		'start':"Hej, jeg hedder Simsimi! Jeg er her for at tale!\n Men tag det roligt, jeg er i udviklingsfasen.",
+		"notcmdia":"Denne kommando er ikke længere tilgængelig, nu kan jeg genkende dit sprog automatisk - send /about vide mere om mig!"
+		}],
+}
+
